27bf10d64c3d552838f90b9ec27048f774a080b2
aerrata <0x0976@gmail.com>
2021-10-07 16:36:36 +0800

    feat: add page

---
+++ resources/views/room/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __("ENTAH") }}</div>
+
+                <div class="card-body">HAAAA</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('styles')
+<style>
+</style>
+@endpush
+
+@push('scripts')
+<script>
+    // $(function () {
+    //     alert()
+    // })
+</script>
+@endpush
