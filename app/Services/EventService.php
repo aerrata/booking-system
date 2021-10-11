607ef90bc9443d01b59e6825c2ebd91e736b1b52
aerrata <0x0976@gmail.com>
2021-10-11 09:32:55 +0800

    feat

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -13,29 +13,30 @@ class EventService
             Carbon::parse($requestData['start_date']),
             Carbon::parse($requestData['end_date']),
         ];
-        
-        $rooms = Room::query()
-        ->where('id', $requestData['room_id'])
-        ->whereHas('bookings', function ($query) use ($times) {
-            $query->whereBetween('start_date', $times)
-                ->orWhereBetween('end_date', $times)
-                ->orWhere(function ($query) use ($times) {
-                    $query->where('start_date', '<', $times[0])
-                    ->where('end_date', '>', $times[1]);
-                });
-        })
-        ->count();
 
-        return ($rooms)?true:false;
+        $rooms = Room::query()
+            ->where('id', $requestData['room_id'])
+            ->whereHas('bookings', function ($query) use ($times) {
+                $query->whereBetween('start_date', $times)
+                    ->orWhereBetween('end_date', $times)
+                    ->orWhere(function ($query) use ($times) {
+                        $query->where('start_date', '<', $times[0])
+                            ->where('end_date', '>', $times[1]);
+                    });
+            })
+            ->count();
+
+        return ($rooms) ? true : false;
     }
 
-    public function isWithinCapacity($requestData){
-       
-        $roomCapacity = Room::query()
-                        ->where('id', $requestData['room_id'])
-                        ->where('capacity', '>', (int)$requestData['participant_total'])
-                        ->count();
+    public function isWithinCapacity($requestData)
+    {
 
-        return ($roomCapacity)?false:true;
+        $roomCapacity = Room::query()
+            ->where('id', $requestData['room_id'])
+            ->where('capacity', '>', (int)$requestData['participant_total'])
+            ->count();
+
+        return ($roomCapacity) ? false : true;
     }
 }
