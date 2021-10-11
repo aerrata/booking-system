f18366da1ea0ad99cdd3f4f7afa726a3901dbbb0
muhaizi <muhaizi@kometsoft.com.my>
2021-10-11 18:57:50 +0800

    fix : unique id

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -59,15 +59,16 @@
                     <td>{{ $room->capacity }}</td>
                     <td>{{ $room->room_category->name }}</td>
                     <td>
-                        <form action="{{ route('room.destroy', $room) }}" method="POST">
+                        <form action="{{ route('room.destroy', $room) }}" method="POST" id="form-room-destroy-{{$room->id}}">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-link" onclick="return confirm('Are you sure?')">Delete</button>
+
                         </form>
                         @if($edit_room)
                             <a class="btn btn-sm btn-link" href="{{ route('room.edit', $room) }}">Edit</a>
                         @endif
                         @if($delete_room)
+                            <button type="submit" class="btn btn-sm btn-link" form="form-room-destroy-{{$room->id}}" onclick="return confirm('Are you sure?')">Delete</button>
                         @endif
                     </td>
                 </tr>
